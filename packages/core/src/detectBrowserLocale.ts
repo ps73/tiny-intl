49e754486b73decddfc30b60ad332e75f7bded9b
Phil <phil.schaffarzyk@gmail.com>
2024-01-29 16:08:13 +0100

    fix: return fallback if navigator is undefined

--- packages/core/src/detectBrowserLocale.ts
+++ packages/core/src/detectBrowserLocale.ts
@@ -4,6 +4,7 @@ export function detectBrowserLocale<T extends string>(options: {
   supportedLocales: T[];
   fallbackLocale: T;
 }): T {
+  if (typeof navigator === 'undefined') return options.fallbackLocale;
   const defaultLocale = navigator?.languages?.[0] || navigator?.language;
   if (!defaultLocale) return options.fallbackLocale;
   return detectLocale(defaultLocale, options);
